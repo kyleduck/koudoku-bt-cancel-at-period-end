5076038ba00450e4514750e5a31af4a1b4298ce9
Romanos Fessas <romanos@fessas.com>
2020-03-31 19:48:33 -0400

    Zero-dollar invoices

--- config/initializers/stripe_event.rb
+++ config/initializers/stripe_event.rb
@@ -12,8 +12,13 @@ StripeEvent.configure do |events|
     invoice = event.data.object
     stripe_id = invoice.customer
     amount = invoice.total.to_f / 100.0
-    subscription = ::Subscription.find_by_stripe_id(stripe_id)
-    subscription.payment_succeeded(amount, invoice.id)
+
+    # Zero-dollar invoices for new customers are marked paid, 
+    # errors out since there's not even a customer yet
+    if invoice.try(:total) > 0
+      subscription = ::Subscription.find_by_stripe_id(stripe_id)
+      subscription.payment_succeeded(amount, invoice.id)
+    end
   end
   
   events.subscribe 'charge.dispute.created' do |event|
